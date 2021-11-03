9b5e0fb1c240f2fd5362333fc9b8734102d3f56f
Brit Wallace <britwallace888@gmail.com>
2021-11-02 21:05:46 -0700

    Added some js to prevent text from disappearing
    Co-authored-by: Julien Leonardo <julien.m.leonardo@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
   $("#formOne").submit(function() {
-    $(".person1").append("blah blah");
-    $(".person2").append("blah blah");
+    $(".fullName").append("input#fullName");
+    $(".address").append("#address");
     $(".animal").append("blah blah");
     $(".exclamation").append("blah blah");
     $(".verb").append("blah blah");
     $(".noun").append("blah blah");
 
     $("#story").show();
+    event.preventDefault();
   });
 });
+  
+
+
